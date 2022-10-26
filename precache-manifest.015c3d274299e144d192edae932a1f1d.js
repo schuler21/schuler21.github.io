59c34cb538d56eeb9de88e48f055103d2c96b1f5
Iurii Nikolaev <uh.nikolaev@mail.ru>
2022-10-26 15:40:35 +0300

    Updates

---
+++ precache-manifest.015c3d274299e144d192edae932a1f1d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2379befd942f88438906",
+    "url": "/my-application/static/css/main.f81ef3be.chunk.css"
+  },
+  {
+    "revision": "2379befd942f88438906",
+    "url": "/my-application/static/js/main.2379befd.chunk.js"
+  },
+  {
+    "revision": "9f98cb25e9a3b8a351ee",
+    "url": "/my-application/static/js/1.9f98cb25.chunk.js"
+  },
+  {
+    "revision": "160d69ddd59909d32845",
+    "url": "/my-application/static/js/runtime~main.160d69dd.js"
+  },
+  {
+    "revision": "d182606ffbaa9381eaa8ac0191bc9378",
+    "url": "/my-application/index.html"
+  }
+];
